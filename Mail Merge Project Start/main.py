be4ec22a98225bae9e34a46d917cfc0d36218bfe
Gyobeom <ikiscc@gmail.com>
2022-02-26 21:47:41 +0900

    using_file

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -6,3 +6,23 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+customer_name = open("./Input/Names/invited_names.txt", "r")
+starting_letter = open("./Input/Letters/starting_letter.txt", "r")
+
+names = customer_name.readlines()
+letter_info = starting_letter.read()
+
+for name in names:
+    new_name = name.strip()
+    change_letter = letter_info.replace("[name]", new_name)
+    new_letter = open(f"./Output/ReadyToSend/letter_for_{new_name}.txt", 'w')
+    new_letter.write(change_letter)
+
+
+
+
+
+
+
